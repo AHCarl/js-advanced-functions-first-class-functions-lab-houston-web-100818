1c2ff593453fbd53c23430321bda79018f63de76
Alexander Carlin <ahcarlin@gmail.com>
2018-11-19 22:34:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const returnLastTwoDrivers = function(drivers){
   return drivers.slice(drivers.length - 2)
 }
 
-const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
